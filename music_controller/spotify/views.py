56868eb3499ec24ae2ab6dcc6fe1f87ef78d417c
Husain-Jinia <husain.jinia0201@gmail.com>
2021-11-09 00:35:33 +0530

    api ep to check spotify authentication

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -48,5 +48,10 @@ def spotify_callback(request, format=None):
     update_or_create_user_tokens(request.session.session_key, access_token,token_type,expires_in,refresh_token)
 
     return redirect('frontend:')
-    
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(self.request.session.session_key)
+        return Response({'status':is_authenticated}, status=status.HTTP_200_OK)
+
 
